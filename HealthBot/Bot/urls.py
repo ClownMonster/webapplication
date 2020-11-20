9d5d99fc52b5923f549dd44a7bd7a5df7db704f3
Manvendra Bansal <bansalmanvendra@gmail.com>
2020-11-21 01:47:42 +0530

    basic pages done

--- HealthBot/Bot/urls.py
+++ HealthBot/Bot/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import landingScreen
+from .views import loginScreen
+from .views import registerScreen
 
 urlpatterns = [
 
     path('',landingScreen,name='landingscreen'),
+    path('login/',loginScreen,name='loginscreen'),
+    path('register/',registerScreen,name = 'registerscreen')
+
 ]
+
